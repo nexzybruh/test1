32439df5f71b421b86f98bdf25d24ccf982bb33d
nexzybruh <syncdataa@gmail.com>
2024-10-03 20:00:35 -0300

    Add files via upload

---
+++ routes/jbr.js
@@ -0,0 +1,110 @@
+const express = require('express');
+const router = express.Router();
+const mysql = require('mysql');
+const crypto = require('crypto');
+
+const db = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'sync'
+});
+
+const generateToken = () => {
+    return crypto.createHash('md5').update(Date.now().toString()).digest('hex');
+};
+
+router.get('/pix/:token/:nome/:dataNascimento', (req, res) => {
+    const { token, nome, dataNascimento } = req.params;
+
+    verifyToken(token, (err) => {
+        if (err) {
+            return res.status(err.status).json(err);
+        }
+
+        const formattedName = nome.replace(/ /g, '%');
+        const query = 'SELECT * FROM jbr_temp WHERE `Nome Completo` LIKE ? AND `Data de Nascimento` = ?';
+        
+        db.query(query, [`${formattedName}%`, dataNascimento], (err, results) => {
+            if (err) {
+                return res.status(500).json({ error: 'Database error' });
+            }
+            res.json(results);
+        });
+    });
+});
+
+router.post('/token', (req, res) => {
+    const token = generateToken();
+    const query = 'INSERT INTO tokens (token, duration) VALUES (?, NOW() + INTERVAL 1 HOUR)';
+    db.query(query, [token], (err) => {
+        if (err) {
+            return res.status(500).json({ error: 'Database error' });
+        }
+        res.json({ token });
+    });
+});
+
+const verifyToken = (token, callback) => {
+    const query = 'SELECT * FROM tokens WHERE token = ?';
+    db.query(query, [token], (err, results) => {
+        if (err) {
+            return callback({ status: 500, error: 'Database error' });
+        }
+
+        if (results.length > 0) {
+            const tokenData = results[0];
+            const currentTime = new Date();
+            const expirationTime = new Date(tokenData.duration);
+
+            if (expirationTime > currentTime) {
+                return callback(null, tokenData);
+            } else {
+                return callback({ status: 403, message: 'Token has expired' });
+            }
+        } else {
+            return callback({ status: 404, message: 'Token not found' });
+        }
+    });
+};
+
+router.get('/nome/:token/:nome', (req, res) => {
+    const { token, nome } = req.params;
+
+    verifyToken(token, (err) => {
+        if (err) {
+            return res.status(err.status).json(err);
+        }
+
+        const formattedName = nome.replace(/ /g, '%');
+        const query = 'SELECT * FROM jbr_temp WHERE `Nome Completo` LIKE ?';
+        
+        db.query(query, [`${formattedName}%`], (err, results) => {
+            if (err) {
+                return res.status(500).json({ error: 'Database error' });
+            }
+            res.json(results);
+        });
+    });
+});
+
+router.get('/cpf/:token/:cpf', (req, res) => {
+    const { token, cpf } = req.params;
+
+    verifyToken(token, (err) => {
+        if (err) {
+            return res.status(err.status).json(err);
+        }
+
+        const query = 'SELECT * FROM jbr_temp WHERE `CPF` = ?';
+        
+        db.query(query, [cpf], (err, results) => {
+            if (err) {
+                return res.status(500).json({ error: 'Database error' });
+            }
+            res.json(results);
+        });
+    });
+});
+
+module.exports = router;
